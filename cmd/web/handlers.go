6a2ffc6a7da753897547d70dc0a47edba28689bf
Jonathan Arnold <jon@mixnblend.com>
2024-04-05 19:46:08 +0200

    refactor: split up server into main and handler files

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -47,25 +46,4 @@ func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 
 	// Then w.Write() method to write the response body as normal.
 	io.WriteString(w, "Save a new snippet...")
-}
-
-func main() {
-	// Use the http.NewServeMux() function to initialise a new servemux, then
-	// register the home function as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	// create a new route which is restricted to POST requests only.
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-	
-	log.Print("starting server on :4000")
-	
-	// Use the http.ListenAndServe() function to start a new web server. We pass in
-  // two parameters: the TCP network address to listen on (in this case ":4000")
-  // and the servemux we just created. If http.ListenAndServe() returns an error
-  // we use the log.Fatal() function to log the error message and exit. Note
-  // that any error returned by http.ListenAndServe() is always non-nil.
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
 }
