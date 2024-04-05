9b9ffb6ee7778b7ecb67865943dca0e25476f998
Jonathan Arnold <jon@mixnblend.com>
2024-04-05 17:33:32 +0200

    feat: add webserver and home catchall route

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// define a home handler function which writes a byte slice containing
+// "Hello from Snippetbox" as the response body.
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	// Use the http.NewServeMux() function to initialise a new servemux, then
+	// register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	
+	log.Print("starting server on :4000")
+	
+	// Use the http.ListenAndServe() function to start a new web server. We pass in
+  // two parameters: the TCP network address to listen on (in this case ":4000")
+  // and the servemux we just created. If http.ListenAndServe() returns an error
+  // we use the log.Fatal() function to log the error message and exit. Note
+  // that any error returned by http.ListenAndServe() is always non-nil.
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
