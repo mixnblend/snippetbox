d695158f9128f70f2dbfaade2c7e85a3ed918688
Jonathan Arnold <jon@mixnblend.com>
2024-04-05 20:05:33 +0200

    feat(ui): add navbar partial

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -22,6 +22,7 @@ func home(w http.ResponseWriter, r *http.Request) {
   // file in the slice.
 	files := []string{
 		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
 		"./ui/html/pages/home.tmpl",
 	}
 
