0bf6fe66ca95b2fa719e61e81908a1ce88452e8f
Jonathan Arnold <jon@mixnblend.com>
2024-04-07 15:10:50 +0200

    feat(db): add create a snippet and retrieve a snippet

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -12,7 +13,7 @@ type Snippet struct {
 	ID      int
 	Title   string
 	Content string
-	created time.Time
+	Created time.Time
 	Expires time.Time
 }
 
@@ -22,13 +23,68 @@ type SnippetModel struct {
 }
 
 // This will insert a new snippet into the database.
-func (m *SnippetModel) Insert(title, string, content string, expires int) (int, error) {
-	return 0, nil
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	// Write the SQL statement we want to execute. I've split it over two lines
+	// for readability (which is why it's surrounded with backquotes instead
+	// of normal double quotes).
+	stmt := `INSERT INTO snippets (title, content, created, expires) 
+	VALUES (?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	// Use the Exec() method on the embedded connection pool to execute the
+	// statement. The first parameter is the SQL statement, followed by the
+	// values for the placeholder parameters: title, content and expiry in
+	// that order. This method returns a sql.Result type, which contains some
+	// basic information about what happened when the statement was executed.
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	// Use the lastInsertId() method on the result to get the ID of our newly inserted
+	// record in the snippets table.
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// The ID returned has the type int64, so we convert it to an int type before returning.
+	return int(id), nil
 }
 
 // This will return a specific snippet based on its id.
 func (m *SnippetModel) Get(id int) (Snippet, error) {
-	return Snippet{}, nil
+
+	// Write the SQL statement we want to execute. Again, I've split it over two
+	// lines for readability.
+	stmt := `SELECT id, title, content, created, expires FROM snippets 
+	WHERE expires > UTC_TIMESTAMP() and id = ?`
+
+	// Use the QueryRow() method on the connection pool to execute our
+	// SQL statement, passing in the untrusted id variable as the value for the
+	// placeholder parameter. This returns a pointer to a sql.Row object which
+	// holds the result from the database.
+	row := m.DB.QueryRow(stmt, id)
+
+	// Initialise a new zeroed Snippet struct.
+	var s Snippet
+
+	// Use row.Scan() to copy the values from each field in sql.Row to the
+	// corresponding field in the Snippet struct. Notice that the arguments
+	// to row.Scan are *pointers* to the place you want to copy the data into,
+	// and the number of arguments must be exactly the same as the number of
+	// columns returned by your statement.
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return Snippet{}, ErrNoRecord
+		} else {
+			return Snippet{}, err
+		}
+	}
+
+	return s, nil
 }
 
 // This will return the 10 most recently created snippets
