a87167152905ed0ea31f42a7fb9e6efbed90af80
Jonathan Arnold <jon@mixnblend.com>
2024-04-07 15:10:50 +0200

    feat(db): add create a snippet and retrieve a snippet

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,24 +1,27 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"io"
 	"net/http"
 	"strconv"
+
+	"github.com/mixnblend/snippetbox/internal/models"
 )
 
 // define a home handler function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body.
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-  // Use the Header().Add() method to add a 'Server: Go' header to the
-  // response header map. The first parameter is the header name, and
-  // the second parameter is the header value.	
+	// Use the Header().Add() method to add a 'Server: Go' header to the
+	// response header map. The first parameter is the header name, and
+	// the second parameter is the header value.
 	w.Header().Add("Server", "Go")
 
 	// Initialize a slice containing the paths to the two files. It's important
-  // to note that the file containing our base template must be the *first*
-  // file in the slice.
+	// to note that the file containing our base template must be the *first*
+	// file in the slice.
 	files := []string{
 		"./ui/html/base.tmpl",
 		"./ui/html/partials/nav.tmpl",
@@ -26,8 +29,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Use the template.ParseFiles() function to read the files and store the
-  // templates in a template set. Notice that we use ... to pass the contents 
-  // of the files slice as variadic arguments.
+	// templates in a template set. Notice that we use ... to pass the contents
+	// of the files slice as variadic arguments.
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		app.serverError(w, r, err)
@@ -35,41 +38,63 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// Then we use the Execute() method on the template set to write the
-  // template content as the response body. The last parameter to Execute()
-  // represents any dynamic data that we want to pass in, which for now we'll
-  // leave as nil.
+	// template content as the response body. The last parameter to Execute()
+	// represents any dynamic data that we want to pass in, which for now we'll
+	// leave as nil.
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-			app.serverError(w, r, err) // Use the serverError() helper.
+		app.serverError(w, r, err) // Use the serverError() helper.
 	}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	// Extract the value of the id wildcard from the request using r.PathValue()
-  // and try to convert it to an integer using the strconv.Atoi() function. If
-  // it can't be converted to an integer, or the value is less than 1, we
-  // return a 404 page not found response.
+	// and try to convert it to an integer using the strconv.Atoi() function. If
+	// it can't be converted to an integer, or the value is less than 1, we
+	// return a 404 page not found response.
 	var invalid bool = (err != nil || id < 1)
 	if invalid {
 		http.NotFound(w, r)
 		return
 	}
 
-	// Use the fmt.Sprintf() function to interpolate the id value with a
-  // message, then write it as the HTTP response.
-	fmt.Fprintf(w, "Display a specific input with ID %d...", id)
+	// Use the SnippetModel's Get() method to retrieve the data for a
+	// specific record based on its ID. If no matching record is found,
+	// return a 404 Not Found response.
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+
+		return
+	}
+
+	// Write the snippet data as a plain-text HTTP response body.
+	fmt.Fprintf(w, "%+v", snippet)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Display a form for creating a new snippet...")
 }
 
-
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	// use the w.WriteHeader() method to send an 201 status code.
-	w.WriteHeader(http.StatusCreated)
+	// Create some variables holding dummy data.  We'll remove these later on during the build.
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
 
-	// Then w.Write() method to write the response body as normal.
-	io.WriteString(w, "Save a new snippet...")
+	// Pass the data to the SnippetModel.Insert() method, receiving the ID of the new record back.
+	id, err := app.snippets.Insert(title, content, expires)
+
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	// Redirect the user to the relevant page for the snippet.
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
