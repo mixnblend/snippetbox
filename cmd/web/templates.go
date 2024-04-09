30a04bad73d9ad92471cb92cffe1484e4861f6eb
Jonathan Arnold <jon@mixnblend.com>
2024-04-09 15:26:55 +0200

    refactor(ui): automatically parse any templates in the partials folder

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -17,39 +17,35 @@ type templateData struct {
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
-	// Initialize a new map to act as the cache.
 	cache := map[string]*template.Template{}
-	// Use the filepath.Glob() function to get a slice of all filepaths that
-	// match the pattern "./ui/html/pages/*.tmpl". This will essentially gives
-	// us a slice of all the filepaths for our application 'page' templates
-	// like: [ui/html/pages/home.tmpl ui/html/pages/view.tmpl]
+
 	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
 	if err != nil {
 		return nil, err
 	}
 
-	// Looper through the page filepaths one by one
 	for _, page := range pages {
-		// Extract the filename (like 'home.tmpl') from the full filepath
-		// and assign it to a variable.
 		name := filepath.Base(page)
 
-		// create a slice containing the filepaths for our base template,
-		// any partials and the page.
-		files := []string{
-			"./ui/html/base.tmpl",
-			"./ui/html/partials/nav.tmpl",
-			page,
-		}
-
-		// Parse the files into a template set
-		ts, err := template.ParseFiles(files...)
+		// Parse the base template file into a template set.
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
 
-		// Add the template set to the map, using the name of the page
-		// (like 'home.tmpl') as the key.
+		// Call ParseGlob() *on this template set* to add any partials.
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		// Call ParseFiles() *on this template set* to add the  page template.
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		// Add the template set to the map as normal...
 		cache[name] = ts
 	}
 
