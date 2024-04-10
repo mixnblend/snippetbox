22dbd86b9364d883c981af8d5bbd9aa0451583e7
Jonathan Arnold <jon@mixnblend.com>
2024-04-10 19:01:38 +0200

    feat(auth): add login

--- internal/models/users.go
+++ internal/models/users.go
@@ -28,13 +28,6 @@ type UserModel struct {
 	DB *sql.DB
 }
 
-// We'll use the Authenticate method to verify whether a user exists with
-// the provided email address and password. This will return the relevant
-// user ID if they do.
-func (m *UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
-}
-
 // We'll use the Exists method to check if a user exists with a specific ID.
 func (m *UserModel) Exists(id int) (bool, error) {
 	return false, nil
@@ -64,4 +57,36 @@ func (m *UserModel) Insert(name, email, password string) error {
 	}
 
 	return nil
+}
+
+func (m *UserModel) Authenticate(email, password string) (int, error) {
+	var id int
+	var hashedPassword []byte
+
+	stmt := `SELECT id, hashed_password FROM users WHERE email = ?`
+
+	// Use the QueryRow() mtehod to select the user details
+	err := m.DB.QueryRow(stmt, email).Scan(&id, &hashedPassword)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	// Check whether the hashed password and plain-text password provided match.
+	// If they don't, we return the ErrInvalidCredentials error.
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	// Otherwise, the password is correct. Return the user ID.
+	return id, nil
 }
