b06ffc1214572482b5a8715dcd8694d2bd3e4a1a
Jonathan Arnold <jon@mixnblend.com>
2024-04-10 11:34:04 +0200

    feat(sessions): add flash message for snippet creation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -136,6 +136,10 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	// Use the Put() method to add a string value ("Snippet successfully
+	// created!") and the corresponding key ("flash") to the session data.
+	app.sessionManager.Put(r.Context(), "flash", "Snippet successfully created!")
+
 	// Redirect the user to the relevant page for the snippet.
 	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
